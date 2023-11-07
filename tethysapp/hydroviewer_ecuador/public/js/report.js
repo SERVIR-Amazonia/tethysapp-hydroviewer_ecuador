c65aad3da0c2efb37d8825355deb14a869386d6d
Juseth Chancay <juseth.chancay@gmail.com>
2023-11-07 16:42:45 -0500

    Fixed image url for PDF report

--- tethysapp/hydroviewer_ecuador/public/js/report.js
+++ tethysapp/hydroviewer_ecuador/public/js/report.js
@@ -27,7 +27,6 @@ const sleep = ms => new Promise(r => setTimeout(r, ms));
 async function convertirAPDF() {
     window.scrollTo({top: 0, behavior: "smooth"});
     await sleep(1000);
-
     const elemento = document.getElementById('report-container-id').innerHTML;
     const pdfConfig = {
       margin: [26, 20, 26, 20], //top, left, buttom, right,
@@ -50,12 +49,15 @@ async function convertirAPDF() {
 
     html2pdf().from(elemento).set(pdfConfig).toPdf().get('pdf').then(function (pdf) {
       var totalPages = pdf.internal.getNumberOfPages(); 
+      console.log(totalPages)
       hh = pdf.internal.pageSize.getWidth() * 0.1
       for (var i = 1; i <= totalPages; i++) {
         pdf.setPage(i);
         // addImage(linkImage, Format, Posicion x, Posicion y, Ancho, Alto)
         pdf.addImage(imgHeader, 'PNG', 0, 0, pdf.internal.pageSize.getWidth(), hh); 
-        pdf.addImage(imgFooter, 'PNG', 0, pdf.internal.pageSize.getHeight() - hh, pdf.internal.pageSize.getWidth(), hh); 
+        pdf.addImage(imgFooter, 'PNG', 0, pdf.internal.pageSize.getHeight() - hh, pdf.internal.pageSize.getWidth(), hh);
       }
+    }).catch((error) => {
+      console.error(`onRejected function called: ${error.message}`);
     }).save("boletin_hidrologico.pdf");
   }
